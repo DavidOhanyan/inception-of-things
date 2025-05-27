e264b46b54ad3093e1e12c281226c7b1da019275
DavidOhanyan <davitohanyan630@gmail.com>
2025-05-27 16:06:42 +0400

    first CD

--- p3/scripts/main.sh
+++ p3/scripts/main.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# echo "setting up docker"
-# ./setup-docker.sh
+echo "setting up docker"
+./setup/setup-docker.sh
 
-# echo "setting up k3d"
-# ./setup-k3d.sh
+echo "setting up k3d"
+./setup/setup-k3d.sh
 
-# echo "setting up argocd"
-# ./setup-argocd.sh
+echo "setting up argocd"
+./setup/setup-argocd.sh
 
-# forwarding on 8080
+echo "applying argocd manifests"
+./sync/apply.sh
+
+echo "forwarding on 8080"
 kubectl port-forward svc/argocd-server 8080:443 --address 0.0.0.0 -n argocd &
+
+echo "forwarding web"
+kubectl port-forward --address 0.0.0.0 svc/app-service 5000:80 -n dev &
 
 # argocd password
 PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
